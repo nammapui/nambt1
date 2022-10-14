b50cdf49c6e078d1000a84306c95a197858c548e
nam <namck21v7k022@vlvh.ctu.edu.vn>
2022-10-14 17:21:29 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app =express();
+app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
 app.get("/",(req,res)=>
